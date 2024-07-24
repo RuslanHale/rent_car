ab240a97bf10e684fcdee41cc8c8cd40ef91bcdf
RuslanHale <ruskhal333@gmail.com>
2024-07-25 00:17:45 +0500

    add deleting, creating, editing car

--- rent_project/rent_app/urls.py
+++ rent_project/rent_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import CarList, CarDetail
+from .views import CarList, CarDetail, CarCreate, CarUpdate, CarDelete
 
 
 urlpatterns = [
-    path("", CarList.as_view(), name= "cars"),
+    path('', CarList.as_view(), name= "cars"),
     path("car/<int:pk>/", CarDetail.as_view(), name="car"),
+    path('car-create/', CarCreate.as_view(), name='car-create'),
+    path("car-update/<int:pk>/", CarUpdate.as_view(), name="car-update"),
+    path('car-delete/<int:pk>/', CarDelete.as_view(), name='car-delete'),
 ]
--- rent_project/rent_app/views.py
+++ rent_project/rent_app/views.py
@@ -3,8 +3,8 @@ from .models import Car
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-# Create your views here.
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class CarList(ListView):
     model = Car
@@ -14,4 +14,23 @@ class CarList(ListView):
 class CarDetail(DetailView):
     model = Car
     context_object_name = "car"
+    fields = "__all__"
     template_name = "rent_app/car.html"
+
+
+class CarCreate(CreateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("cars")
+
+
+class CarUpdate(UpdateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("cars")
+
+
+class CarDelete(DeleteView):
+    model = Car
+    context_object_name = "car"
+    success_url = reverse_lazy("cars")
